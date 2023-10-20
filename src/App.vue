043fad809d6c995d4adfa6629b1f1e9e9cb32ab3
DriFFCBDB <adriellibarboza.1105@aluno.saojudas.br>
2023-10-20 15:48:49 -0300

    feat: suffle e http request

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 v-html="this.question"></h1>
+
+    <input type="radio" name="options" value="True">
+    <label>True</label><br>
+
+    <input type="radio" name="options" value="False">
+    <label>False</label><br>
+
+    <button class="send" type="button">Enviar</button>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+// https://opentdb.com/api.php?amount=1&category=22&difficulty=easy
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      question: undefined,
+      incorrect_answers:undefined,
+      correct_answer: undefined 
+    }   
+  },
+
+  computed: {
+    answers(){
+
+      var answers = JSON.parse(JSON.stringify(this.incorrect_answers));
+      
+      answers.splice( Math.round(Math.random() * answers.lenght), 0, this.correct_answer);
+   
+      return answers;
+    }
+  },
+
+  created(){
+      this.axios
+      .get("https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=boolean")
+      .then((response) => {
+        this.question = response.data.results[0].question;
+        this.incorrect_answers = response.data.results[0].incorrect_answers;
+        this.correct_answer = response.data.results[0].correct_answer;
+      })
   }
 }
 </script>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+}
+
+h1 {
+  margin-top: 40px;
+}
+
+input[type='radio']{
+  margin: 12px 4px;
+}
+
+button.send {
+  margin-top: 12px;
+  height: 40px;
+  min-width: 120px;
+  padding: 0 16px;
+  color: #fff;
+  background-color: #1867c0;
+  border: 1px solid #1867c0;
+  cursor: pointer;
+}
+
+section.score {
+  border-bottom: 1px solid black;
+  padding: 24px;
+  font-size: 18px;
+
+  span {
+    padding: 8px;
+    font-weight: bold;
+    border: 1px solid black;
+  }
 }
 </style>
